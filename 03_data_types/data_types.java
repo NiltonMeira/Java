e6f13e0f4102f3a12f134196ec056247c3b76126
Nilton <niltonprudys@gmail.com>
2023-10-24 21:05:54 -0300

    Versão 01, Adicionado arquivos, Hello word, Variables e Data types

---
+++ 03_data_types/data_types.java
@@ -0,0 +1,18 @@
+class Data_type
+{
+    public static void main(String a[])
+    {
+        float marks = 6.5f;
+        double nilton = 7.5;
+        char c = 'k';
+        boolean b = true;
+        long dsd = 4425254l;
+        
+        System.out.println(marks);
+        System.out.println(nilton);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(dsd);
+
+    }
+}
